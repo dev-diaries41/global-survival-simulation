19f4345af199d491952c550db88b0aa41dc84226
d41dev <d41dev@proton.me>
2025-01-19 11:21:31 +0000

    update package.json

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-import { SurvivalSimulation, saveFile  } from "./src";
+import { SurvivalSimulation} from "./src";
 
 
 (async () => {
     const sim = new SurvivalSimulation()
     console.log(sim)
     const result = await sim.run();
-    saveFile(JSON.stringify(result, null, 2), `sim_result_${Date.now()}.json`);
 })();
