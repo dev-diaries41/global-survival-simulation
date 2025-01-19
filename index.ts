ea7212f678b5792d83638540c0c3f1d1701cc74a
d41dev <d41dev@proton.me>
2025-01-19 21:00:54 +0000

    update package fs

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { SurvivalSimulation} from "./src";
-
-
-(async () => {
-    const sim = new SurvivalSimulation()
-    console.log(sim)
-    const result = await sim.run();
-})();
---
+++ src/examples.ts
@@ -0,0 +1,8 @@
+// import { SurvivalSimulation} from ".";
+
+
+// (async () => {
+//     const sim = new SurvivalSimulation()
+//     console.log(sim)
+//     const result = await sim.run();
+// })();
